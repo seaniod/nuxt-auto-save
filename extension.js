f62ab70e0a1e5b5513bccb48a7c50323198034f9
seaniod <sean@odonoghuelab.org>
2025-05-31 16:20:24 +1000

    Initial commit

--- extension.js
+++ extension.js
@@ -1,21 +1,17 @@
 const vscode = require("vscode");
 
 function activate(context) {
-  let disposable = vscode.commands.registerCommand("extension.pushWithHook", function () {
-    const terminal = vscode.window.createTerminal("Git Push");
-    terminal.show(true);
-    terminal.sendText("git push");
-  });
-  let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-  statusBarItem.text = "$(cloud-upload) Push with Hook";
-  statusBarItem.command = "extension.pushWithHook";
-  statusBarItem.tooltip = "Run git push with pre-push hook";
-  statusBarItem.show();
-  context.subscriptions.push(statusBarItem);
-  // Register the command
-  context.subscriptions.push(disposable);
+  const interval = setInterval(() => {
+    vscode.workspace.textDocuments.forEach((doc) => {
+      if (doc.fileName.includes("/.nuxt/") && doc.isDirty) {
+        console.log("Polling: auto-saving", doc.fileName);
+        doc.save();
+      }
+    });
+  }, 1000); // every second
+
+  context.subscriptions.push({dispose: () => clearInterval(interval)});
 }
-vscode.window.showInformationMessage("Push with Hook extension is active");
 
 function deactivate() {}
 
